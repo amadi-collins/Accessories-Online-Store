b96b0f1fd1f8cd9518f2a3bd211727a54a2d1634
Amadi Collins <info@amadi.tech>
2025-11-23 18:33:01 +0300

    Refactor header structure and fix HTML syntax

--- includes/Header.php
+++ includes/Header.php
@@ -4,27 +4,37 @@ require_once dirname(__FILE__).'/../core/url.php';
 
 $LOGO_PATH =  base_url('assets/images/logo.jpg');
 
-$header = <<<HTML
+ob_start();
+include_once dirname(__FILE__).'/../api/products/Filter.php';
+
+$Filters = ob_get_clean();
+
+$header = "
     <header>
-        <div class="header flex ac sb">
-            <div class="logo"><img src="$LOGO_PATH" alt="Logo"></div>
-            <div class="nav">
+        <div class=\"header flex ac sb\">
+            <div class=\"logo\"><img src=\"$LOGO_PATH\" alt=\"Logo\"></div>
+            <div class=\"nav\">
                 <nav>
-                    <ul class="flex ac">
-                        <li>Home</li>
-                        <li>Products</li>
-                        <li>Contact</li>
-                        <li>Location</li>
+                    <ugitl class=\"flex ac\">
+                        <li><span>Home</span></li>
+                        <li>
+                            <span>Products</span>
+                            <nav class=\"sub-menu\">
+                                $Filters
+                            </nav>
+                        </li>
+                        <li><span>Contact</span></li>
+                        <li><span>Location</span></li>
                     </ul>
                 </nav>
             </div>
-            <div class="icons flex ac" style="gap: 10px">
-                <div><i class="bi bi-bag"></i></div>
-                <div class="esmac-d btn flex ac ctr">Get Started</div>
+            <div class=\"icons flex ac\" style=\"gap: 10px\">
+                <div><i class=\"bi bi-bag\"></i></div>
+                <div class=\"esmac-d btn flex ac ctr\">Get Started</div>
             </div>
         </div>
     </header>
-HTML;
+";
 
 echo $header;
 ?>
